4f9cf9f2976efaa7e70624f0d5e24d9f99115590
Adam Crawford <adamcrawford253@gmail.com>
2023-11-07 18:39:13 -0800

    Remove the extra list operations.

--- expand_data.py
+++ expand_data.py
@@ -1,11 +1,13 @@
 import csv
 
-with open("input/data.csv", 'r') as csv_read_file:
+with open("input/data.csv", "r") as csv_read_file:
     reader = csv.reader(csv_read_file)
     first_line = next(reader)
-    with open("output/output.csv", 'w', newline='') as csv_write_file:
+    with open("output/output.csv", "w", newline="") as csv_write_file:
         writer = csv.writer(csv_write_file)
         writer.writerow(first_line)
         for row in reader:
-            for i in range(int(row[4])):
-                writer.writerow(row[:4] + [""] + [row[5]])
+            num_times_to_add = int(row[4])
+            row[4] = ""
+            for i in range(num_times_to_add):
+                writer.writerow(row)
